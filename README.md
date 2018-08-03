1b56439a71f8a492606aef68ac5290052004bc24
zacanger <zac@zacanger.com>
2018-08-03 11:01:23 -0600

    Add shit

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
 # zrgs
 
+**WIP**
+
+Tiny cli args module
+
 --------
 
 ## Installation
 
+`npm i zrgs`
+
 ## Usage
+
+```javascript
+const { hasFlag, args } = require('zrgs')
+
+if (hasFlag('help')) {
+  // display help
+} else if (hasFlag('something')) {
+  handleSomething(args)
+}
+```
 
 ## License
 
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = {}
+const argIn = (arg) => (opts) => opts.includes(arg)
+const handleAbbrev = (s) => [ `--${s}`, `-${s}`, `-${s.charAt(0)}` ]
+const args = process.argv.slice(2)
+const first = args[0]
+const incl = argIn(first)
+const hasFlag = (flag) => incl(handleAbbrev(flag))
+
+module.exports = {
+  hasFlag,
+  args
+}
