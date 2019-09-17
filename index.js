e2de7cf7079bc46522af7ce788de2bfa6a6f2bb8
zacanger <zac@zacanger.com>
2019-09-17 14:02:38 -0600

    chore: standard --fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const argIn = (arg) => (opts) => opts.includes(arg)
-const handleAbbrev = (s) => [ `--${s}`, `-${s}`, `-${s.charAt(0)}` ]
+const handleAbbrev = (s) => [`--${s}`, `-${s}`, `-${s.charAt(0)}`]
 const args = process.argv.slice(2)
 const first = args[0]
 const incl = argIn(first)
